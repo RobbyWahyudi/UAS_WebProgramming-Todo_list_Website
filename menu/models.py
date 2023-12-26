00f65eed247f89b410400018ad2facc396043ee3
RobbyWahyudi <mazrobby04@gmail.com>
2023-12-26 11:22:12 +0700

    Menambahkan tabel Task dan Kategori

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Kategori(models.Model):
+    nama = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nama
+
+
+class Task(models.Model):
+    judul = models.CharField(max_length=200)
+    deskripsi = models.TextField()
+    tanggal_jatuh_tempo = models.DateField()
+    status = models.CharField(max_length=20)
+    kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.judul
